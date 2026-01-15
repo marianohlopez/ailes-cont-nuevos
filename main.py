697b0caaf0341ad03ebfbe888b8ff89e00d9bedc
Mariano Lopez <ml.3012@gmail.com>
2026-01-15 10:50:45 -0300

    production fix main

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from db import connect_db
 from extract import extract_mail_pas
 from transform import gen_mails_pas
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MAIL_DESTINO = os.getenv("MAIL_DESTINO")
-MAIL_DESTINO_2 = os.getenv("MAIL_DESTINO_2")
 
 def main():
 
